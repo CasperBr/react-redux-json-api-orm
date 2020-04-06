da5bbdfab959fa31eddda39befa46ec46dd7c084
Casper Broertjes <casperbroertjes@hotmail.com>
2020-04-06 12:29:26 +0200

    index

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/resource.core';
 export * from './lib/resource.dispatcher';
 export * from './lib/resource.reducer';
 export * from './lib/resource.decorators';
+export * from './lib/resource.hooks';
