2a4584153047c087d1e95a17806c1a8876815ab2
Casper Broertjes <casperbroertjes@hotmail.com>
2020-04-06 11:53:38 +0200

    Enable all features

--- src/lib/resource.core.ts
+++ src/lib/resource.core.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import { inject, injectable } from 'inversify';
+import { HttpRequestBuilder } from './httpRequestBuilder';
 
 export interface IRequestParams {
   filter?: string
@@ -40,5 +41,24 @@ export class Resource {
     let includesString = [...this.toOne, this.toMany].join(',');
     if (includesString.length === 1) return;
     return includesString;
+  }
+
+  public hydrate(store: any, type: string, payload: any) {
+    store.dispatch({ type, payload });
+  }
+  
+  public static hydrate(store: any, type: string, payload: any) {
+    store.dispatch({ type, payload });
+  }
+
+  public asyncAction(store: any, request: any) {
+    Resource.asyncAction(store, request);
+  }
+
+  public static asyncAction(store: any, request: any) {
+    HttpRequestBuilder.jsonApiRequest(request)
+      .then(payload => {
+        store.dispatch({ type: `${request.action}_SUCCESS`, payload });
+      });
   }
 }
